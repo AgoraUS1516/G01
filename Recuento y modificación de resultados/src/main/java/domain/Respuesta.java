fe50c37ff34b7c979a41b9819fcc18b0b3b7bc34
juacordia <Juanoide@users.noreply.github.com>
2015-11-16 13:58:37 +0000

    He añadido cosas en diferentes clases que faltaban, como entitys, relaciones y he modificado el populatedatabase para añadir beans

--- Recuento y modificación de resultados/src/main/java/domain/Respuesta.java
+++ Recuento y modificación de resultados/src/main/java/domain/Respuesta.java
@@ -1,17 +1,25 @@
 package domain;
 
-public class Respuesta {
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public class Respuesta extends DomainEntity{
 
 	private String pregunta;
 	private int numerosSi;
 	private int numerosNo;
 	
+	//No pueden tener constructores
+	/*
 	public Respuesta(String question){
 		pregunta = question;
 		numerosNo = 0;
 		numerosSi = 0;
 	}
-
+*/
 	public String getPregunta() {
 		return pregunta;
 	}
--- Recuento y modificación de resultados/src/main/java/domain/Votacion.java
+++ Recuento y modificación de resultados/src/main/java/domain/Votacion.java
@@ -1,16 +1,22 @@
 package domain;
 
+import java.util.Collection;
 import java.util.List;
 
+import javax.persistence.Access;
+import javax.persistence.AccessType;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 //Sustituto de Votos y/o VotosCifrados
 @Entity
+@Access(AccessType.PROPERTY)
 public class Votacion extends DomainEntity{
 
 	private Integer msg;
-	private List<Resultado> resultados;
+	
 
 	public Integer getMsg() {
 		return this.msg;
@@ -19,7 +25,13 @@ public class Votacion extends DomainEntity{
 	public void setMsg(Integer msg) {
 		this.msg = msg;
 	}
-	@OneToMany
+	
+	// Relationships ----------------------------------------------------------
+	private List<Resultado> resultados;
+	
+	@NotNull
+	@Valid
+	@OneToMany(mappedBy="votacion")
 	public List<Resultado> getResultados() {
 		return resultados;
 	}
@@ -28,6 +40,8 @@ public class Votacion extends DomainEntity{
 		this.resultados = resultados;
 	}
 
+	
+	
 
 
 }
