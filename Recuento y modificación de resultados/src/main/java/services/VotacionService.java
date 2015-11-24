77c2954023e2c589fd4fc051a82ca25399079786
Lammerlord92 <lammerlord92@gmail.com>
2015-11-24 21:03:45 +0100

    Cambiado el servicio para hacer el recuento, en teoría, falta comprobarlo. Dejado método completo para recontar (Recuento). Requiere completar con el JSON y lo que nos entra.

--- Recuento y modificación de resultados/src/main/java/services/VotacionService.java
+++ Recuento y modificación de resultados/src/main/java/services/VotacionService.java
@@ -10,13 +10,23 @@
 
 package services;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import domain.Answer;
+import domain.Opcion;
+import domain.Resultado;
 import domain.Votacion;
+import domain.Voto;
 import repositories.VotacionRepository;
 
 @Service
@@ -53,6 +63,68 @@ public class VotacionService {
 	}
 	
 	// Business methods -------------------------------------------------------
+	public Collection<Resultado> recuentaVotos(Votacion votacion){
+		Collection<Voto> votos=votacion.getVotos();
+		Collection<Resultado> res=new ArrayList<Resultado>();
+		Map<String,List<Opcion>> rec=new HashMap<String,List<Opcion>>();
+		for(Voto v:votos){
+			for(Answer a:v.getAns()){
+				if(rec.containsKey(a.getQuestion())){
+					List<Opcion> opciones=rec.get(a.getQuestion());
+					if(opciones==null){
+						Opcion o=new Opcion();
+						o.setOpcion(a.getAnswer_question());
+						o.setVotos(1);
+						o.setAns(a);
+						List<Opcion>lopciones= new ArrayList<Opcion>();
+						lopciones.add(o);
+						rec.put(a.getQuestion(), opciones);
+					}else{
+						boolean contains=false;
+						for(Opcion o:opciones){
+							if(o.getOpcion().equals(a.getAnswer_question())){
+								contains=true;
+								Opcion nO=new Opcion();
+								nO.setOpcion(o.getOpcion());
+								nO.setVotos(o.getVotos()+1);
+								nO.setAns(a);
+								opciones.remove(o);
+								opciones.add(nO);					
+							}
+						}if(!contains){
+							Opcion o=new Opcion();
+							o.setOpcion(a.getAnswer_question());
+							o.setVotos(1);
+							o.setAns(a);
+							List<Opcion>lopciones= new ArrayList<Opcion>();
+							lopciones.add(o);
+							rec.put(a.getQuestion(), opciones);
+						}
+						
+					}
+				}
+			}
+		}
+		for(String s:rec.keySet()){
+			Resultado r=new Resultado();
+			r.setPregunta(s);
+			r.setOpciones(rec.get(s));
+			res.add(r);
+		}
+		return res;
+	}
+	
+	
+	public (String) Recuento(int votacionId){
+		//Llamada a la votación de los métodos cifrados
+		//Hacer una votación transformando los votos cifrados en votos normales
+		//Meter los votos a una nueva votación
+		recuentaVotos(votacion);
+		//Convertir a json la votación
+		//devolver JSON
+		
+	}
+	
 	
 
 }
